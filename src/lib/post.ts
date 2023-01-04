7dd4ef0ce16dce65939aa8be4c626cf6786083c1
x1aotian <fangxt@uw.edu>
2023-01-03 22:17:27 -0800

    modify

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -58,7 +58,7 @@ export async function getPost(slug: string): Promise<Post> {
 	const source = await serialize(content, {
 		scope: data,
 		mdxOptions: {
-			rehypePlugins: [[RehypeAutolinkHeadings, {}]],
+			rehypePlugins: [[RehypeAutolinkHeadings as any, {}]],
 			remarkPlugins: [RemarkCodeTitles, RemarkEmoji, RemarkPrism, RemarkSlug],
 		},
 	});
